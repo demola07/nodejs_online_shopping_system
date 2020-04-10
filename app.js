57f217ffec81ed791bf224bdec9e77081ea8b8cd
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-10 12:39:28 +0100

    Created a minimal backend structure using MVC pattern

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const errorController = require('./controllers/error');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
 
 app.listen(3000, () => {
   console.log('PORT RUNNING on 3000');
