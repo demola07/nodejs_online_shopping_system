e77ec6bb0ca4b6166126a543a4d989c11ad21aef
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-11 20:29:56 +0100

    created cart and cart-item relations

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 const sequelize = require('./utils/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -36,9 +38,14 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 sequelize
-  // .sync({ force: true })
-  .sync()
+  .sync({ force: true })
+  // .sync()
   .then((result) => {
     return User.findByPk(1);
   })
