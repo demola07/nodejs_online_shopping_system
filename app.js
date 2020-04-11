a4b297a780a2b74bdbf5ab6be088b19b931262af
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-11 20:43:44 +0100

    worked on getting products in cart

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
     return User.findByPk(1);
   })
@@ -57,6 +57,9 @@ sequelize
   })
   .then((user) => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000, () => {
       console.log('PORT RUNNING on 3000');
     });
