9df4f64a3498c78fe4c883a710e96aa8e27e4e60
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-14 04:17:58 +0100

    Fixed error with add products page

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,15 +1,13 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
-    .then((products) => {
-      res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/products',
-      });
-    })
-    .catch((err) => console.log(err));
+  Product.findAll().then((products) => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products',
+    });
+  });
 };
 
 exports.getProduct = (req, res, next) => {
