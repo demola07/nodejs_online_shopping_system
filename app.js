396dc1080d56543579011705d6b6f9817431aa98
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-15 12:57:33 +0100

    Created user schema in mongoose and dummy user in database

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('views', 'views');
 
 const errorController = require('./controllers/error');
 const mongoose = require('mongoose');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -17,14 +17,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5e950398072ed912ccbf9067')
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('5e96f4f63818a8203091a8d6')
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,6 +36,18 @@ mongoose
     'mongodb+srv://demola:demola@cluster0-yxizt.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: 'john',
+          email: 'john@john.com',
+          cart: {
+            items: [],
+          },
+        });
+      }
+      user.save();
+    });
     app.listen(3000, () => {
       console.log('PORT RUNNING on 3000');
     });
