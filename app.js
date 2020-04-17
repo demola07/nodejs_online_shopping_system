195ceb4806e546063fd55a20e5daf4ede7f0de7d
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-17 20:31:49 +0100

    adding csrf protection

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const csrf = require('csurf');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,6 +19,8 @@ const store = new mongoDBStore({
   collection: 'sessions',
 });
 
+const csrfProtection = csrf();
+
 const User = require('./models/user');
 const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
@@ -27,6 +30,7 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }));
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -40,6 +44,11 @@ app.use((req, res, next) => {
     .catch((err) => console.log(err));
 });
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
--- controllers/shop.js
+++ controllers/shop.js
@@ -35,7 +35,7 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        isAuthenticated: req.session.isLoggedIn,
+        // isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));
