a44a2f197771a166e2791a1f4be5001c9580b517
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-17 20:57:44 +0100

    Providing user feedback using flash message

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const csrf = require('csurf');
+const flash = require('connect-flash');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -31,6 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }));
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
