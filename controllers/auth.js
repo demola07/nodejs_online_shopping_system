b2e302035445007474312f064cfd30848001879a
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-18 11:50:21 +0100

    Added logic to update reset usr password

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,8 +5,6 @@ const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 require('dotenv').config();
 
-console.log(process.env.SENDGRID_API_KEY);
-
 const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
@@ -89,10 +87,10 @@ exports.postSignup = (req, res, next) => {
       }
       return bcrypt
         .hash(password, 12)
-        .then((hashPassword) => {
+        .then((hashedPassword) => {
           const user = new User({
             email: email,
-            password: hashPassword,
+            password: hashedPassword,
             cart: { items: [] },
           });
           return user.save();
@@ -168,4 +166,56 @@ exports.postReset = (req, res, next) => {
         console.log(err);
       });
   });
+};
+
+exports.getNewpassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      let message = req.flash('error');
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect('/login');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
