3d9e76d65bfe816655ed6ba5f3a4d66475ee18c9
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-20 10:59:03 +0100

    Filtering files by mimetype

--- app.js
+++ app.js
@@ -32,6 +32,18 @@ const fileStorage = multer.diskStorage({
   },
 });
 
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 const User = require('./models/user');
 const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
@@ -39,7 +51,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single('image'));
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }));
